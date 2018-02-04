fd35fc47a889de567a8852d2f12aa333d8166dc0
Anna Royer <anna.royer@colorado.edu>
2018-02-04 00:11:12 -0700

    Add set company before action method and refactor methods accordingly

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,16 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: [:show, :edit]
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_action :set_company, except: [:show, :destroy]
+
   def index
-    @company = Company.find(params[:company_id])
     @jobs = @company.jobs
   end
 
   def new
-    @company = Company.find(params[:company_id])
     @job = Job.new()
   end
 
   def create
-    @company = Company.find(params[:company_id])
     @job = @company.jobs.new(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
@@ -29,9 +28,9 @@ class JobsController < ApplicationController
   end
 
   def update
-    Job.update(params[job_params])
+    @job.update(job_params)
 
-    redirect_to @job
+    redirect_to company_job_path(@company, @job)
   end
 
   def destroy
@@ -46,5 +45,9 @@ class JobsController < ApplicationController
 
   def set_job
     @job = Job.find(params[:id])
+  end
+
+  def set_company
+    @company = Company.find(params[:company_id])
   end
 end
