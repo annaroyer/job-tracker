2fb3c1746b9f2e0d1f217190347618929bab2cf2
Anna Royer <anna.royer@colorado.edu>
2018-02-04 15:52:12 -0700

    pass tests

--- spec/features/companies/user_deletes_a_company_spec.rb
+++ spec/features/companies/user_deletes_a_company_spec.rb
@@ -5,6 +5,8 @@ describe "User deletes existing company" do
     company = Company.create(name: "ESPN")
     visit companies_path
 
+    save_and_open_page
+
     within(".company_#{company.id}") do
       click_link "Delete"
     end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,11 +3,22 @@ require 'rails_helper'
 describe Category, type: :model do
   describe 'validations' do
     it 'is invalid without a title' do
-      category_1 = Category.create!(title: 'Education')
-      category_2 = Category.create!
+      category = Category.new
 
-      expect(category_1).to be_valid
-      expect(category_2).to be_invalid
+      expect(category).to be_invalid
+    end
+
+    it 'is valid with a title' do
+      category = Category.create!(title: 'Education')
+
+      expect(category).to be_valid
+    end
+
+    it 'has a unique name' do
+      Category.create!(title: 'Education')
+      category = Category.new(title: 'Education')
+
+      expect(category).to be_invalid
     end
   end
 end
