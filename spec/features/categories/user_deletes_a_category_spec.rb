a7fcf861f6d5af9468c122bcb858e42a47876376
Anna Royer <anna.royer@colorado.edu>
2018-02-04 17:26:06 -0700

    Test user can delete a category from category index

---
+++ spec/features/categories/user_deletes_a_category_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  context 'when I click delete on a category in the category index' do
+    it 'I can delete that category' do
+      category_1 = Category.create!(title: 'Education')
+      category_2 = Category.create!(title: 'Web Development')
+
+      visit categories_path
+      expect(page).to have_content(category_1.title)
+      expect(page).to have_content(category_2.title)
+
+      within '.category_2' do
+        click_on 'Delete'
+      end
+
+      expect(current_path).to eq(categories_path)
+      expect(page).to have_content(category_1.title)
+      expect(page).to_not have_content(category_2.title)
+    end
+  end
+end
