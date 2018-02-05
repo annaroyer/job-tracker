baf7b34b3d14309b981a5b20e53d9a898e081127
Anna Royer <anna.royer@colorado.edu>
2018-02-04 20:14:45 -0700

    add categories to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,18 @@
 Company.destroy_all
 Job.destroy_all
+Category.destroy_all
 
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+CATEGORIES = ["Web Development", "Education", "Finance", "Health Care", "Quality Assurance", "Hospitality", "Administrative", "Child Care", "Customer Service", "Research"]
 
-COMPANIES.each do |name|
+COMPANIES.zip(CATEGORIES).each do |name, title|
   company = Company.create!(name: name)
+  category = Category.create!(title: title)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample)
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category: category)
     puts "  Created #{company.jobs[num].title}"
   end
 end
