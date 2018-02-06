1140d2365317a6750d1d6bba4a5aeb1dc8f8cbba
Anna Royer <anna.royer@colorado.edu>
2018-02-06 08:52:13 -0700

    add create action and whitelisted params to contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,13 @@
 class ContactsController < ApplicationController
+  def create
+    @contact = Contact.new(contact_params)
+    flash[:success] = "Contact #{@contact.full_name} created!" if @contact.save
+    redirect_to company_contact_path(@contact.company, @contact)
+  end
 
+  private
+
+  def contact_params
+    params.require(:contact).permit(:full_name, :position, :email)  
+  end
 end
