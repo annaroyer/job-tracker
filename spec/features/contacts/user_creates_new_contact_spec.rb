64181469c7e63975377e598dca0834a364e3d59a
Anna Royer <anna.royer@colorado.edu>
2018-02-05 23:11:47 -0700

    add user creates a contact spec

---
+++ spec/features/contacts/user_creates_new_contact_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  context 'when I visit a company page and fill in the info for a contact' do
+    it 'I can create a new contact for that company' do
+      company = Company.create!(name: 'ESPN')
+
+      visit company_path(company)
+      fill_in 'contact[full_name]', with: 'Betty Sang'
+      fill_in 'contact[position]', with: 'Producer'
+      fill_in 'contact[email]', with: 'bspro@espn.org'
+      click_on 'submit'
+
+      expect(page).to have_content('Betty Sang')
+      expect(page).to have_content('Producer')
+      expect(page).to have_content('bspro@espn.org')
+    end
+  end
+end
