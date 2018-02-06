269f1fd1a383ac5c1d0d9442d496ed9eefb6642b
Anna Royer <anna.royer@colorado.edu>
2018-02-06 15:39:46 -0700

    add instance variable company and change path in create method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,14 @@
 class ContactsController < ApplicationController
   def create
-    @contact = Contact.new(contact_params)
+    @company = Company.find(params[:company_id])
+    @contact = @company.contacts.new(contact_params)
     flash[:success] = "Contact #{@contact.full_name} created!" if @contact.save
-    redirect_to company_contact_path(@contact.company, @contact)
+    redirect_to company_path(@company)
   end
 
   private
 
   def contact_params
-    params.require(:contact).permit(:full_name, :position, :email)  
+    params.require(:contact).permit(:full_name, :position, :email)
   end
 end
