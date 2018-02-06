bd2514e96512a90c794cf64575a0d3ab0fe3ab9c
Anna Royer <anna.royer@colorado.edu>
2018-02-05 17:44:03 -0700

    add test it has a list of all its jobs

--- spec/features/categories/user_sees_a_category_spec.rb
+++ spec/features/categories/user_sees_a_category_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 describe 'As a user' do
   context 'when I visit a category show' do
-    it 'I can see information about that category' do
+    it "I can see it's title and all jobs with that category" do
+      company = Company.create!(name: 'New Hights Elementary')
       category = Category.create!(title: 'Education')
+      category.jobs.create!(title: 'Teacher', description: 'fulfilling', level_of_interest: '60', city: 'Denver', company: company)
+      category.jobs.create!(title: 'Administrator', description: 'fulfilling',  level_of_interest: '60', city: 'Denver', company: company)
+      category.jobs.create!(title: 'Paraprofessional', description: 'fulfilling',  level_of_interest: '60', city: 'Denver', company: company)
 
       visit category_path(category)
 
       expect(page).to have_content('Education')
+      expect(page).to have_link('Teacher')
+      expect(page).to have_link('Administrator')
+      expect(page).to have_link('Paraprofessional')
     end
   end
 end
