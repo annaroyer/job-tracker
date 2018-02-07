1b4fc4966fc1591ba0e7c0472d3f1a0216825e75
Anna Royer <anna.royer@colorado.edu>
2018-02-07 16:09:45 -0700

    add method to calcu average interest level on all jobs

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,8 @@ class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :jobs, dependent: :destroy
   has_many :contacts, dependent: :destroy
+
+  def average_interest_level
+    jobs.average(:level_of_interest)
+  end
 end
