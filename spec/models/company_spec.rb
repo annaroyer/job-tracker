00aec40b3d24fe898b86c42193a36860118addc9
Anna Royer <anna.royer@colorado.edu>
2018-02-08 06:58:58 -0700

    add test for top intest level class method

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -13,4 +13,22 @@ describe Company do
     it { should have_many :jobs }
     it { should have_many :contacts }
   end
+
+  describe 'class methods' do
+    context '.top_average_interest_level' do
+      it 'returns the 3 companies with the highest average interest level in order' do
+        company_1 = create(:company)
+        company_2 = create(:company)
+        company_3 = create(:company)
+        company_4 = create(:company)
+        create_list(:job, 3, company: company_1, level_of_interest: 50)
+        create_list(:job, 3, company: company_2, level_of_interest: 60)
+        create_list(:job, 3, company: company_3, level_of_interest: 80)
+        create_list(:job, 2, company: company_1, level_of_interest: 100)
+        create(:job, company: company_4, level_of_interest: 90)
+
+        expect(Company.top_average_interest_level).to eq([company_4, company_3, company_1])
+      end
+    end
+  end
 end
