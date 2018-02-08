c133831ab173999362d8de68d5e91a8a5a67bdaf
Anna Royer <anna.royer@colorado.edu>
2018-02-08 06:20:26 -0700

    fix job index test to test it shows all jobs

--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
-describe "User sees all jobs" do
-  scenario "a user sees all the jobs for a specific company" do
-    company = Company.create!(name: "ESPN")
-    category_1 = Category.create!(title: 'sports analytics')
-    category_2 = Category.create!(title: 'Quality Assurance')
-    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category_1)
-    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City", category: category_2)
+describe "As a user" do
+  context 'when I visit jobs index' do
+    scenario "I can see all jobs" do
+      jobs = create_list(:job, 3)
 
-    visit company_path(company)
+      visit jobs_path
 
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("QA Analyst")
+      jobs.each do |job|
+        expect(page).to have_link(job.title)
+      end
+    end
   end
 end
